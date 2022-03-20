3ca3e234f05bd14cda0088f90c43d2d7279233c3
leedd87 <leedav87@gmail.com>
2022-03-20 11:10:15 +0900

    llamada a fake API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import CrudApi from "./components/CrudApi";
 import CruddApp from "./components/CrudApp";
 
 function App() {
 	return (
 		<>
 			<h1>Ejercicios con React</h1>
+			<CrudApi />
+			<hr />
 			<CruddApp />
 			<hr />
 		</>
